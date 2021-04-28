da86f33f1373f6756ff405358f1fb3f2926a5d00
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-04-28 22:19:11 +0000

    [:seedling:] task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    number = len(argv) - 1
+    import sys
+    number = len(sys.argv) - 1
     if number is not 1:
         if number is 0:
             print("{} arguments.".format(number))
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     else:
         print("{} argument:".format(number))
 
-    for count, ch in enumerate(argv):
-        if count > 0:
-            print("{}: {}".format(count, ch))
+    for i in range(1, len(sys.argv)):
+        ch = sys.argv[i]
+        print("{}: {}".format(i, ch))
