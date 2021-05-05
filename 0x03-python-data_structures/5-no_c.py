9b8870dafab67f5153e5bde02400e3a1c230896c
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-05-05 22:57:13 +0000

    [ADD] task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    string = ''
+    new_str = ''
     for i in my_string:
         if i != 'c' and i != 'C':
-            string += i
-    return string
+           new_str += i
+    return new_str
