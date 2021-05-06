44f7208767e0b122f4a3878c642b16554bf8a5c4
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-05-06 23:11:37 +0000

    [ADD] task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        key = max(a_dictionary.keys())
-        return key
+        return max(a_dictionary, key=a_dictionary.get)
     return None
