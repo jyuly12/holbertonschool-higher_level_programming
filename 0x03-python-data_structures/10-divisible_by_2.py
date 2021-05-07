06b2d0a094ffffc976512fde13403275d7968e6c
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-05-07 00:03:19 +0000

    [FIX] parameters

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def divisible_by_2(my_list=[]):
     new_list = []
     if my_list:
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_str = ""
     for i in my_string:
         if i != 'c' and i != 'C':
-           new_str += i
+            new_str += i
     return new_str
