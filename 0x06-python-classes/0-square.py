ab3f6623958d6181fadbfa5a0da2c3517c14d70e
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-05-18 22:04:23 +0000

    [ADD] task 0-3

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Create a empty class Square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    Define Square class
+    """
+    def __init__(self, size):
+        """
+        Initialize Square class
+
+        Args:
+            size (int): size of square.
+        """
+        self.__size = size
