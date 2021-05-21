cc52a8321a42c1736b79d4a8b8fe85ad31a510e5
jyuly12 <78006686+jyuly12@users.noreply.github.com>
2021-05-20 20:48:03 -0500

    [FIX] a little fix

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,10 @@
- #!/usr/bin/python3
- """
- This module creates a empty class
- """
- 
-  class Rectangle:
+#!/usr/bin/python3
+"""
+This module creates a empty class
+"""
+
+
+class Rectangle:
     """
     Defines rectangle class
     """
