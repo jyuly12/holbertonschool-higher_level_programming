36334afe718f249b4b03509f8ec707b2abc33f14
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-06-01 04:04:44 +0000

    [ADD] task incomplete

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines pascal_triangle function
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+
+    return triangle
