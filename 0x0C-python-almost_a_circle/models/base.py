9e370c0476c489839c3ed54dc706bfbaaf2dd42f
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-06-08 23:16:35 +0000

    [FIX] PEP8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,8 +17,8 @@ class Base:
         Args:
         id (int)
         """
-        if id != None:
-            self.id=id
+        if id is not None:
+            self.id = id
         else:
             Base.__nb_objects += 1
-            self.id=Base.__nb_objects
+            self.id = Base.__nb_objects
