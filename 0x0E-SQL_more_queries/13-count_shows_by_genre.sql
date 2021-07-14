5763b8558222fce93eccd100d268aa029f3c8850
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-07-14 02:39:55 +0000

    [Update] file 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,7 @@
 -- First column must be called genre.
 -- Second column must be called number_of_shows.
 -- Results must be sorted in descending order by the number of shows linked.
-SELECT genre.name,COUNT(*) AS number_of_shows
-FROM genre INNER JOIN tv_show_genres ON gender.id = tv_show_genres.genre_id
+SELECT tv_genres.name,COUNT(*) AS number_of_shows
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genders.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.name ORDER BY number_of_shows DESC;
