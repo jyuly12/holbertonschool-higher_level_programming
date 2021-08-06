63656b79ff742cf93696af34fc63bc40e11cef01
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-08-06 04:25:20 +0000

    [Add] python files , README and sql script

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N) from the database.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    sql = "SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC"
+    sql = sql.format(argv[4])
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    for item in rows:
+        print(item)
+    cursor.close()
+    db.close()
