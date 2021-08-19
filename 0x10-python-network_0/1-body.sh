8d611f0fcd93d582f7c6f411bb2537f9a9a66e89
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-08-19 17:00:49 -0500

    Update file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL, and displays the body 
-curl -sLf "$1"
+# takes in a URL, sends a GET request to the URL, and displays the body 
+curl -sL "$1"
