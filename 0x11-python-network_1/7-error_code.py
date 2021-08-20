4d86c2f881406097fb6f0e7be1185052d22a1108
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-08-20 18:47:06 -0500

    Add mandatory task

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the body of the response.
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+    response = requests.get(argv[1])
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
